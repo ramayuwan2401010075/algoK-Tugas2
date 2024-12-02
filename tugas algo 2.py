09782903693179d3a67bdd64a8380bf251a78888
ramayuwan2401010075 <ramayuwan099@gmail.com>
2024-12-02 20:38:44 +0800

    Add files via upload

---
+++ tugas algo 2.py
@@ -0,0 +1,11 @@
+print("Konversi meter ke centimeter")
+
+#memasukan nilai meter yang dinginkan user
+meter = float (input("Masukan nilai (m): "))
+
+#proses mengonversi meter ke centimeter
+centimeter = meter * 100
+hasil =("Panjang dalam centimeter:" + str(centimeter) +  "cm")
+
+#hasil konversi
+print(hasil)
